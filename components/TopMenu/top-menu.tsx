6cc2e2c223838b0d64af9089b6befdae672b6875
Lucas Henrique <lucashenriquesd@gmail.com>
2024-12-15 22:17:18 -0300

    feat: add navigation menu and dark theme

---
+++ components/TopMenu/top-menu.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export default function TopMenu() {
+  return (
+    <header className="flex sticky top-0 w-full justify-center items-center h-11">
+      <div className="flex justify-center items-center min-w-80">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Menu Item
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Menu Item
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </header>
+  );
+}
